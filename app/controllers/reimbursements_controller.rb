0e9f1a09286a1055b91448de4e8d59369a0f0a8f
Justin Mauldin <justin.mauldin7@gmail.com>
2023-06-22 10:14:22 -0600

    Addes the create method with reimbursement_params method to the ReimbursementsController to create the record.

--- app/controllers/reimbursements_controller.rb
+++ app/controllers/reimbursements_controller.rb
@@ -1,6 +1,22 @@
 class ReimbursementsController < ApplicationController
     def new
     end
+
+    def create
+        @reimbursement = Reimbursement.new(reimbursement_params)
+
+        if @reimbursement.save
+            redirect_to @reimbursement
+        else
+            render "new"
+        end
+    end
+
+    private
+
+    def reimbursement_params
+        params.permit(:cost_city_1, :start_date_1, :end_date_1, :cost_city_2, :start_date_2, :end_date_2, :cost_city_3, :start_date_3, :end_date_3, :cost_city_4, :start_date_4, :end_date_4)
+    end
 end
 
     
