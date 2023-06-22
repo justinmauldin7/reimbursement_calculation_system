fa1393467df656ddb8ae572bb6c48c3e7e64a13f
Justin Mauldin <justin.mauldin7@gmail.com>
2023-06-22 14:34:17 -0600

    Adds the format_reimbursement method to the ReimbursementCalculator PORO so that we get only the information we care about.

--- app/models/reimbursement_calculator.rb
+++ app/models/reimbursement_calculator.rb
@@ -1,5 +1,27 @@
 class ReimbursementCalculator
     def self.execute(reimbursement)
-        
+        reimbursement_hash = reimbursement.attributes
+        formatted_reimbursement = format_reimbursement(reimbursement_hash)
+    end
+
+    private
+
+    def self.format_reimbursement(reimbursement_hash)
+        # Remove the fields we do not care about
+        reimbursement_hash.delete('id')
+        reimbursement_hash.delete('created_at')
+        reimbursement_hash.delete('updated_at')
+
+        reimbursement_hash.map do |key, value|
+            # make sure that every value is a string for the below logic 
+            # since we deal with other data types.
+            value = value.to_s
+
+            if value.nil? || value.empty?
+                reimbursement_hash.delete(key)
+            end
+        end
+
+        return reimbursement_hash
     end
 end
