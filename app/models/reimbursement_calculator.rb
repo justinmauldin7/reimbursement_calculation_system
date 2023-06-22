f697cb19a75f25a0362843bc96e1a29774e168df
Justin Mauldin <justin.mauldin7@gmail.com>
2023-06-22 16:46:26 -0600

    Adds the format_reimbursement method to the ReimbursementCalculator so each project is seperated from each other.

--- app/models/reimbursement_calculator.rb
+++ app/models/reimbursement_calculator.rb
@@ -1,7 +1,8 @@
 class ReimbursementCalculator
     def self.execute(reimbursement)
         reimbursement_hash = reimbursement.attributes
-        formatted_reimbursement = clean_up_reimbursement(reimbursement_hash)
+        cleaned_up_reimbursement = clean_up_reimbursement(reimbursement_hash)
+        formatted_reimbursment = format_reimbursement(cleaned_up_reimbursement)
     end
 
     private
@@ -23,5 +24,28 @@ class ReimbursementCalculator
         end
 
         return reimbursement_hash
+    end
+
+    def self.format_reimbursement(cleaned_up_reimbursement)
+        project_set_hash = {}
+        i = 1
+        # We divide by 3 here to calculate the "iteration_number" because that is 
+        # how many key value pairs relate to each project.
+        iteration_number = (cleaned_up_reimbursement.keys.count / 3) + 1
+       
+        while i < iteration_number  do
+            project_hash = {}
+
+            cleaned_up_reimbursement.each do |key, value|
+                if key.include?(i.to_s)
+                    project_hash[key] = value
+                end
+            end
+            project_set_hash[i] = project_hash
+
+            i += 1
+        end
+
+        return project_set_hash
     end
 end
